0faba965eedb8cbc8b8cf92511b0eed5aac0deec
Elis Ramadanova <117296291+eramadanova@users.noreply.github.com>
2025-02-09 20:53:42 +0200

    Update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """
-Main entry point of the application.
-
-This module initializes and runs the Flask application.
-It creates the application context and sets up the database.
+    Main entry point of the application.
+    
+    This module initializes and runs the Flask application.
+    It creates the application context and sets up the database.
 """
 
 from src import create_app, db
